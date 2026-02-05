43c6809dffc00b347841eb9db5757123701d53ec
Craig White <gerzytet@gmail.com>
2026-02-05 15:18:31 -0500

    add template

---
+++ quiz_game_template.py
@@ -0,0 +1,16 @@
+import requests
+import random
+from bs4 import BeautifulSoup
+
+# download this page:
+url = r'https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_category'
+
+# Step 1: parse the page
+
+# Step 2: find the table
+
+# Step 3: extract data from the table
+
+# Step 4: turn the data into a better form for processing
+
+# Step 5: create quiz game logic
